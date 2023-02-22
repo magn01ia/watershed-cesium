0aa68a3a99ee98502c64045aa81b54d005a9e68e
magn01ia <84883260+magn01ia@users.noreply.github.com>
2023-02-22 17:52:28 +0900

    Plateauの試験配信terrainを使用

--- docs/script.js
+++ docs/script.js
@@ -1,5 +1,8 @@
+Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI5N2UyMjcwOS00MDY1LTQxYjEtYjZjMy00YTU0ZTg5MmViYWQiLCJpZCI6ODAzMDYsImlhdCI6MTY0Mjc0ODI2MX0.dkwAL1CcljUV7NA7fDbhXXnmyZQU_c-G5zRx8PtEcxE";
     const viewer = new Cesium.Viewer('cesiumContainer', {
-      terrainProvider: Cesium.createWorldTerrain(),
+      terrainProvider: new Cesium.CesiumTerrainProvider({
+        url: Cesium.IonResource.fromAssetId(770371)
+      }),
       animation: false,
       baseLayerPicker: false,
       fullscreenButton: true,
@@ -8,8 +11,12 @@
       navigationHelpButton: false,
       sceneModePicker: false,
       timeline: false,
-    });    
-
+      imageryProvider: new Cesium.UrlTemplateImageryProvider({
+        url: 'https://cyberjapandata.gsi.go.jp/xyz/seamlessphoto/{z}/{x}/{y}.jpg',
+        credit: '<a href="https://maps.gsi.go.jp/development/ichiran.html">地理院タイル</a>'
+      })
+    });
+  
     Cesium.GeoJsonDataSource.clampToGround = true;
     const wb = viewer.dataSources.add(Cesium.GeoJsonDataSource.load("data/WatershedBoundary.geojson",{
       fill:Cesium.Color.DEEPSKYBLUE.withAlpha(0.1),
